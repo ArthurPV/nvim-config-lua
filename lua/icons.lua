cd43f8809c4cb028cde4b81d457dea4eac75800f
Arthur <arthurpontiervalois@gmail.com>
2020-12-28 00:30:26 -0500

    Update icons

--- lua/icons.lua
+++ lua/icons.lua
@@ -1,11 +1,5 @@
 require'nvim-web-devicons'.setup {
-	override = {
-		zsh = {
-			icon = "",
-			color = "#A3A8E7",
-			name = "Zsh"
-		}
-	};
+	-- you can modify icons by default
 
 	default = true; -- enable icons (default to false) 
 }
