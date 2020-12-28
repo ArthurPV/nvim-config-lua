d90864d5667b2072c1f721f7e661e0e43eeb1112
Arthur <arthurpontiervalois@gmail.com>
2020-12-28 00:28:01 -0500

    Add icons (zsh)

---
+++ lua/icons.lua
@@ -0,0 +1,11 @@
+require'nvim-web-devicons'.setup {
+	override = {
+		zsh = {
+			icon = "",
+			color = "#A3A8E7",
+			name = "Zsh"
+		}
+	};
+
+	default = true; -- enable icons (default to false) 
+}
